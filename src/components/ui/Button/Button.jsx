f25621c81cec4eeada9cee84a65001708da02efd
champix56 <desorbaix@free.fr>
2024-11-20 16:15:09 +0100

    mise en oeuvre de props

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 
 function Button(props) {
+ console.log(props);
  
     return (
-        <TouchableOpacity><Text>Bejamin</Text></TouchableOpacity>
+        <TouchableOpacity>
+            <Text>{props.text}</Text>
+        </TouchableOpacity>
     );
 }
 export default Button;
