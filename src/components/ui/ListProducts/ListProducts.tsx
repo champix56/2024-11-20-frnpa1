e9f8e4ec656704a5897f258a7f480d2038a9982c
champix56 <desorbaix@free.fr>
2024-11-21 13:55:25 +0100

    format

--- src/components/ui/ListProducts/ListProducts.tsx
+++ src/components/ui/ListProducts/ListProducts.tsx
@@ -1,16 +1,18 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import styles from './ListProducts.styles';
-import { IProduct } from '../../../interfaces/IProduct';
+import {IProduct} from '../../../interfaces/IProduct';
 
 interface IListProductsProps {
-  produit:IProduct
+  produit: IProduct;
 }
 
 const ListProducts: React.FC<IListProductsProps> = props => {
   return (
     <View style={styles.ListProducts}>
-      <Text>{props.produit.id}:{props.produit.name}</Text>
+      <Text>
+        {props.produit.id}:{props.produit.name}
+      </Text>
     </View>
   );
 };
