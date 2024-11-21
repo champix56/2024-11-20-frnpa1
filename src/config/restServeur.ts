edde67700061da5eed892135bafeec49d621d1a3
champix56 <desorbaix@free.fr>
2024-11-22 00:40:31 +0100

    fichier de constantes

---
+++ src/config/restServeur.ts
@@ -0,0 +1,4 @@
+export const REST_HOST = 'localhost';
+export const REST_PORT = '5679';
+export const REST_ADRESSE = `http://${REST_HOST}:${REST_PORT}`;
+export const RESSOURCES = Object.freeze({products: '/products'});
