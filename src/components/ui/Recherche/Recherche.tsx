be174c97f6ebbeb5fb6db831955141f4e00cd821
champix56 <desorbaix@free.fr>
2024-11-21 14:35:08 +0100

    gestion d'etat

--- src/components/ui/Recherche/Recherche.tsx
+++ src/components/ui/Recherche/Recherche.tsx
@@ -1,13 +1,23 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+import React, {useState} from 'react';
 import styles from './Recherche.styles';
 
 interface IRechercheProps {}
 
 const Recherche: React.FC<IRechercheProps> = props => {
+  const [inputValue, setInputValue] = useState('');
   return (
     <View style={styles.Recherche}>
-      <Text>Recherche</Text>
+      <TextInput
+        placeholder="Saisie de recheche"
+        onChangeText={t => {
+          setInputValue(t);
+          console.log(t);
+        }}
+      />
+      <Text style={{backgroundColor: 'skyblue', color: 'tomato'}}>
+        {inputValue}
+      </Text>
     </View>
   );
 };
