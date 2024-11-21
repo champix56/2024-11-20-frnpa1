4e9300c29cb67ccae9d3f5dec0efc5cf8ebc8580
champix56 <desorbaix@free.fr>
2024-11-21 17:15:09 +0100

    ajout click on product

--- src/components/ui/ListProducts/ListProducts.tsx
+++ src/components/ui/ListProducts/ListProducts.tsx
@@ -1,19 +1,22 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableHighlight} from 'react-native';
 import React from 'react';
 import styles from './ListProducts.styles';
 import {IProduct} from '../../../interfaces/IProduct';
 
 interface IListProductsProps {
   produit: IProduct;
+  onProductPressed: Function;
 }
 
 const ListProducts: React.FC<IListProductsProps> = props => {
   return (
-    <View style={styles.ListProducts}>
-      <Text>
+    <TouchableHighlight
+      style={styles.ListProducts}
+      onPress={() => props.onProductPressed(props.produit.id)}>
+      <Text style={{fontSize: 25}}>
         {props.produit.id}:{props.produit.name}
       </Text>
-    </View>
+    </TouchableHighlight>
   );
 };
 
